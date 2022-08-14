8875b1099130c3f0818569daf78abffcdd07e010
Sebastian Iacob <iacob1832@gmail.com>
2022-08-14 14:48:35 +0100

    Integrate twillio
    Integrate twillio to send sms.

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,20 +1,41 @@
 import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
-OWM_URL = "https://api.openweathermap.org/data/2.5/onecall"
-API_KEY = "your_open_weather_key_here"  # Your API key
-MY_LAT = 50.942062   # Your latitude
-MY_LONG = -2.633308  # Your longitude
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("TWILLIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILLIO_AUTH_TOKEN")
 
-parameters = {
-    "lat": MY_LAT,
-    "lon": MY_LONG,
-    "appid": API_KEY,
-    "exclude": "current,minutely,daily",
-    "hourly": True
+weather_params = {
+    "lat": "50.942062",
+    "lon": "-2.633308",
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
-response = requests.get(url=OWM_URL, params=parameters)
+response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
-data = response.json()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
 
-print(data["hourly"][0]["weather"][0]["id"])
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+            body="It's going to rain today. Remember to bring an ☔️",
+            from_="+12534476674",
+            to=os.environ.get("PERSONAL_PHONE_NUMBER")
+        )
+    print(message.status)
