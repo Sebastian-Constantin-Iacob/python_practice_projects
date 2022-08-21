91d27dfa90f80be8f120f80953b65ef4803db6f9
Sebastian Iacob <iacob1832@gmail.com>
2022-08-21 18:09:33 +0100

    Get up and coming python events
    Create a list of up and coming python events, using selenium

--- selenium_projects/web_scraper/main.py
+++ selenium_projects/web_scraper/main.py
@@ -1,1 +1,16 @@
-chrome_driver_path = "YOUR_CHROMEDRIVER_PATH"
+# Get a list of up and coming events and the dates
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+THE_URL = "https://www.python.org/"
+main_dict = {}
+
+chrome_driver_path = "/Users/constantin-sebastianiacob/Chrom Driver/chromedriver"
+driver = webdriver.Chrome(chrome_driver_path)
+
+driver.get(THE_URL)
+item = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[2]/div[2]/div/ul')
+value_list = item.text.split("\n")
+print(value_list)
+
+driver.quit()
