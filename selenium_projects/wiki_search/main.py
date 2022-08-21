5f57fce087a011a7161eb409f41bd27ed200c5f3
Sebastian Iacob <iacob1832@gmail.com>
2022-08-21 18:29:30 +0100

    Do wiki search using selenium

--- selenium_projects/wiki_search/main.py
+++ selenium_projects/wiki_search/main.py
@@ -1,15 +1,15 @@
-# Get a list of up and coming events and the dates
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-THE_URL = "https://www.python.org/"
+THE_URL = "https://www.wikipedia.org/"
 
 chrome_driver_path = "/Users/constantin-sebastianiacob/Chrom Driver/chromedriver"
 driver = webdriver.Chrome(chrome_driver_path)
 
 driver.get(THE_URL)
-item = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[2]/div[2]/div/ul')
-value_list = item.text.split("\n")
-print(value_list)
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
 
 driver.quit()
