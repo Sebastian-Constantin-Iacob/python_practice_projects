7116b7b2ecfa31d44384b1dae3128aef3a11ef71
Sebastian Iacob <iacob1832@gmail.com>
2022-09-17 08:01:40 +0100

    Completed markup integration with static

--- flask_projects/personal_blog/main.py
+++ flask_projects/personal_blog/main.py
@@ -8,5 +8,20 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
