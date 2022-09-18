3c551d72621fdd1db7361e1bdac09d973ca6c499
Sebastian Iacob <iacob1832@gmail.com>
2022-09-18 08:38:46 +0100

    Enabled full decided functionality

--- flask_projects/personal_blog/main.py
+++ flask_projects/personal_blog/main.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+import requests
+import datetime
+from post import Post
 
 app = Flask(__name__)
+post_obj = []
+
+
+def get_posts():
+    response = requests.get(url="https://api.npoint.io/c623ef892d16ec605190", verify=False)
+    response.raise_for_status()
+    post_collection = response.json()
+    for post in post_collection:
+        post['date'] = datetime.datetime.strptime(post['date'], '%Y%m%d').strftime('%d / %m / %Y')
+        post_o = Post(post_id=post['post_id'], title=post['title'], subtitle=post['subtitle'], author=post['author'],
+                      date=post['date'], post_text=post['post_text'])
+        post_obj.append(post_o)
+    return post_obj
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', post_list=get_posts())
 
 
 @app.route('/about.html')
@@ -18,9 +34,13 @@ def contact():
     return render_template('contact.html')
 
 
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
+@app.route('/post/<int:numx>')
+def get_post(numx):
+    requested_post = None
+    for post in post_obj:
+        if int(post.p_id) == numx:
+            requested_post = post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == '__main__':
---
+++ flask_projects/personal_blog/post.py
@@ -0,0 +1,9 @@
+class Post:
+    def __init__(self, post_id, title, subtitle, author, date, post_text):
+        self.p_id = post_id
+        self.p_title = title
+        self.p_subtitle = subtitle
+        self.p_author = author
+        self.p_date = date
+        self.p_text = post_text
+
